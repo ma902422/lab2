28f57e93a488aaddf9c9dc65caeaaf5a2a857305
ma902422 <ma902422@gmail.com>
2019-05-10 23:31:13 +0500

    first commit

---
+++ src/rules/RulesOf6005Test2.java
@@ -0,0 +1,14 @@
+package rules;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RulesOf6005Test2 {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
